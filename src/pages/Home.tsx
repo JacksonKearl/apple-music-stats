c2e0d042a60c3ebab027e0e707c4a02e04a8db32
Jackson Kearl <jackson.kearl@outlook.com>
2023-07-13 04:55:56 -0700

    pad

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -308,14 +308,14 @@ export function Home() {
         minPlays={minPlays}
         data={data}
       ></App>
-      <div>
+      <p style={"padding: 5px"}>
         This uses <a href="https://www.chartjs.org/">Chart.js</a>,{" "}
         <a href="https://react-chartjs-2.js.org/">react-chartjs-2</a>, and{" "}
         <a href="https://preactjs.com/">preact</a>!{" "}
         <a href="https://github.com/JacksonKearl/apple-music-stats">
           View Source
         </a>
-      </div>
+      </p>
     </>
   )
 }
